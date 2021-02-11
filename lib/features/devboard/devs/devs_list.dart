05ff8b04dcdf3fac10e3dbc7e94c6ab599638043
Celesica <j.enelya13@gmail.com>
2021-02-11 09:48:33 +0800

    remove

--- lib/features/devboard/devs/devs_list.dart
+++ lib/features/devboard/devs/devs_list.dart
@@ -1,6 +1,5 @@
 import 'package:devs/core/constant/string.dart';
 import 'package:devs/core/models/dev.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
